a026b18ba2dc85cd247238bb49a01d6c8936cb75
Anupheaus <tony.hales@moodinternational.com>
2019-03-01 09:16:06 +0000

    Updated the binder helpers

--- src/binder/binder.ts
+++ src/binder/binder.ts
@@ -1,14 +1,7 @@
-import * as React from 'react';
 import { IMap } from 'anux-common';
 
-declare module 'react' {
-
-  interface IBinderOptions {
-    key?: string;
-  }
-
-  function binder<TFunc extends Function>(target: Object, value: TFunc, options?: IBinderOptions): TFunc;
-
+interface IBinderOptions {
+  key?: string;
 }
 
 interface IBinderEntry {
@@ -18,31 +11,26 @@ interface IBinderEntry {
 
 const binders = new WeakMap<any, IMap<IBinderEntry>>();
 
-Object.defineProperty(React, 'binder', {
-  value<TFunc extends Function>(target: Object, value: TFunc, options: React.IBinderOptions = {}) {
-    let memory: IMap<IBinderEntry> = binders.get(target);
-    const key = options.key || value.toString();
+export function binder<TFunc extends Function>(target: Object, value: TFunc, options: IBinderOptions = {}) {
+  let memory: IMap<IBinderEntry> = binders.get(target);
+  const key = options.key || value.toString();
 
-    if (!memory) {
-      memory = {};
-      binders.set(target, memory);
-    }
+  if (!memory) {
+    memory = {};
+    binders.set(target, memory);
+  }
 
-    if (!memory[key]) {
-      memory[key] = {
-        binderFunc: (...args: any[]) => {
-          const func = memory[key].delegateFunc;
-          return func(...args);
-        },
-        delegateFunc: null,
-      };
-    }
-    const data = memory[key];
-    data.delegateFunc = value; // update the bound data with the new function
+  if (!memory[key]) {
+    memory[key] = {
+      binderFunc: (...args: any[]) => {
+        const func = memory[key].delegateFunc;
+        return func(...args);
+      },
+      delegateFunc: null,
+    };
+  }
+  const data = memory[key];
+  data.delegateFunc = value; // update the bound data with the new function
 
-    return data.binderFunc as TFunc; // return the bound function every time
-  },
-  enumerable: false,
-  configurable: true,
-});
-// React['binder'] = ;
+  return data.binderFunc as TFunc; // return the bound function every time
+}
