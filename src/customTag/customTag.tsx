cf4b153acd317322804f3a723a8cacf0900a604f
Anupheaus <tony.hales@moodinternational.com>
2019-03-26 17:11:23 +0000

    Fixed some issues with the customTag

--- src/customTag/customTag.tsx
+++ src/customTag/customTag.tsx
@@ -1,23 +1,24 @@
-import { FunctionComponent, useContext, createElement } from 'react';
+import { useContext, createElement, forwardRef } from 'react';
 import { CustomTagContext, ICustomTagContext } from './context';
 
 interface IProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
   name: string;
 }
 
-export const CustomTag: FunctionComponent<IProps> = ({ name, children, ...rest }) => {
+export const CustomTag = forwardRef<HTMLElement, IProps>(({ name, children, ...rest }, ref) => {
   const { prefix = '' } = useContext(CustomTagContext) || {} as ICustomTagContext;
   const separator = prefix.length > 0 ? '-' : '';
   name = `${prefix}${separator}${name}`;
 
   rest = {
     class: rest['class'] || rest.className,
-    ...(({ className, ...innerRest }) => innerRest)(rest),
+    ...rest,
   } as any;
+  delete rest.className;
 
   return createElement(
     name,
-    { key: name, ...rest },
+    { key: name, ...rest, ref },
     children,
   );
-};
+});
