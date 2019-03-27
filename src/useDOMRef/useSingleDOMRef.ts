eef9a3b1e846407248beae94610445f4bded3413
Anupheaus <tony.hales@moodinternational.com>
2019-03-27 21:57:07 +0000

    Added no config option to single DOM ref

--- src/useDOMRef/useSingleDOMRef.ts
+++ src/useDOMRef/useSingleDOMRef.ts
@@ -1,5 +1,5 @@
 import { useReusableDOMRef } from './useReusableDOMRef';
-import { useRef } from 'react';
+import { useRef, RefObject } from 'react';
 import { HTMLTargetDelegate } from './models';
 
 interface IUseSingleDOMConfig {
@@ -7,16 +7,26 @@ interface IUseSingleDOMConfig {
   disconnected?(element: HTMLElement): void;
 }
 
-export function useSingleDOMRef(config: IUseSingleDOMConfig): HTMLTargetDelegate {
-  const { connected, disconnected } = {
-    connected: Function.empty(),
-    disconnected: Function.empty(),
-    ...config,
-  };
-  const keyRef = useRef(Math.uniqueId());
-  return useReusableDOMRef({
-    connected: (_key, _data, element) => connected(element),
-    disconnected: (_key, _data, element) => disconnected(element),
-  })(keyRef.current);
-
+export function useSingleDOMRef(): [RefObject<HTMLElement>, HTMLTargetDelegate];
+export function useSingleDOMRef(config: IUseSingleDOMConfig): HTMLTargetDelegate;
+export function useSingleDOMRef(config?: IUseSingleDOMConfig): any {
+  if (!config) {
+    const elementRef = useRef<HTMLElement>();
+    const delegate = useSingleDOMRef({
+      connected: element => elementRef.current = element,
+      disconnected: () => elementRef.current = undefined,
+    });
+    return [elementRef, delegate];
+  } else {
+    const { connected, disconnected } = {
+      connected: Function.empty(),
+      disconnected: Function.empty(),
+      ...config,
+    };
+    const keyRef = useRef(Math.uniqueId());
+    return useReusableDOMRef({
+      connected: (_key, _data, element) => connected(element),
+      disconnected: (_key, _data, element) => disconnected(element),
+    })(keyRef.current);
+  }
 }
