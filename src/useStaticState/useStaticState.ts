aa79f84f09c06435c9f2e9e651f6335c2d83a1ee
Tony Hales <tony.hales@moodinternational.com>
2019-04-03 15:20:21 +0100

    Extended useStaticState

--- src/useStaticState/useStaticState.ts
+++ src/useStaticState/useStaticState.ts
@@ -4,9 +4,13 @@ import { is, DeepPartial } from 'anux-common';
 type SetStaticStateAction<S> = DeepPartial<S> | ((prevState: S) => S);
 export type SetStaticState<TState extends {}> = Dispatch<SetStaticStateAction<TState>>;
 
+export type OnUpdateDelegate<TState extends {}> = (state: TState, prevState: TState) => TState;
+export type OnUpdateRegister<TState extends {}> = (delegate: OnUpdateDelegate<TState>) => void;
+
 interface IStateWrapper<TState extends {}> {
   values: TState;
   proxy: TState;
+  callbacks: OnUpdateDelegate<TState>[];
 }
 
 function updateProxy<TState extends {}>(state: IStateWrapper<TState>) {
@@ -21,19 +25,32 @@ function updateProxy<TState extends {}>(state: IStateWrapper<TState>) {
   removed.forEach(key => { delete state.proxy[key]; });
 }
 
-export function useStaticState<TState extends {}>(initialState: TState): [TState, SetStaticState<TState>] {
+export function useStaticState<TState extends {}>(initialState: TState): [TState, SetStaticState<TState>, OnUpdateRegister<TState>] {
   const state = useRef<IStateWrapper<TState>>({
     values: initialState,
     proxy: undefined,
+    callbacks: [],
   });
+  state.current.callbacks = [];
+
   const update: SetStaticState<TState> = stateOrDelegate => {
-    state.current.values = is.function(stateOrDelegate) ? stateOrDelegate(state.current.values) : Object.merge({}, state.current.values, stateOrDelegate);
+    let prevValues = state.current.values;
+    let values = is.function(stateOrDelegate) ? stateOrDelegate(state.current.values) : Object.merge({}, state.current.values, stateOrDelegate);
+    state.current.callbacks.forEach(callback => {
+      values = callback(values, prevValues);
+      prevValues = values;
+    });
+    state.current.values = values;
     updateProxy(state.current);
     return state.current.proxy as TState;
   };
+
+  const addCallback: OnUpdateRegister<TState> = (delegate: OnUpdateDelegate<TState>): void => { state.current.callbacks.push(delegate); };
+
   if (!state.current.proxy) {
     state.current.proxy = {} as TState;
     updateProxy(state.current);
   }
-  return [state.current.proxy as TState, update];
+
+  return [state.current.proxy as TState, update, addCallback];
 }
