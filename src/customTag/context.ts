57259a95f6832df6d194822f52c1f74233ad502c
Anupheaus <tony.hales@moodinternational.com>
2019-04-05 16:06:10 +0100

    Fixed some build issues

--- src/customTag/context.ts
+++ src/customTag/context.ts
@@ -1,7 +1,7 @@
-import { createContext } from 'react';
+import { createContext, Context } from 'react';
 
 export interface ICustomTagContext {
   prefix: string;
 }
 
-export const CustomTagContext = createContext({ prefix: '' });
+export const CustomTagContext: Context<ICustomTagContext> = createContext<ICustomTagContext>({ prefix: '' });
