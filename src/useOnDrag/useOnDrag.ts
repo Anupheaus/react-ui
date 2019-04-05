e966f45908f8639629ecff0395531a54ea2f724a
Anupheaus <tony.hales@moodinternational.com>
2019-04-05 15:18:30 +0100

    Changed useOnDrag to use useStaticState

--- src/useOnDrag/useOnDrag.ts
+++ src/useOnDrag/useOnDrag.ts
@@ -1,21 +1,57 @@
 import { IUseOnDragResult, IUseOnDragConfig } from './models';
 import { ICoordinates } from 'anux-common';
-import { useState, useRef, Dispatch, SetStateAction } from 'react';
 import { useBound } from '../useBound';
 import { useOnUnmount } from '../useOnUnmount';
-import { useSingleDOMRef, HTMLElementRef, HTMLTargetDelegate } from '../useDOMRef';
+import { useSingleDOMRef } from '../useDOMRef';
 import { setDraggingData } from '../dragAndDropRegistry/registry';
-import { useToggleState, IUseToggleStateResult } from '../useToggleState';
+import { useStaticState, SetStaticState } from '../useStaticState';
+
+type IConfig = IUseOnDragConfig<any, any>;
+type SetState = SetStaticState<IState>;
 
 interface IState {
   isDragging: boolean;
+  config: IConfig;
+  dragTarget: HTMLElement;
+  classTarget: HTMLElement;
+  isUnmounted: boolean;
+  mouseDownHandler(event: MouseEvent): void;
+  removeHandlers(): void;
 }
 
-function applyClassToElement(applyDraggingClassTo: HTMLElement, classToApply: string, isDragging: boolean): void {
-  // apply the class to the element
-  if (!applyDraggingClassTo || !classToApply) { return; }
-  const method = isDragging ? 'add' : 'remove';
-  applyDraggingClassTo.classList[method](classToApply);
+function applyDefaults(config: IConfig): IConfig {
+  return {
+    classToApplyWhileDragging: 'is-dragging',
+    data: undefined,
+    isDisabled: false,
+    threshold: 3,
+    onDragStart: () => null,
+    onDrag: ({ passthroughData }) => passthroughData,
+    onDragEnd: () => void 0,
+    ...config,
+  };
+}
+
+function handleStateUpdated(state: IState, prevState: IState): IState {
+  let { config: { isDisabled, classToApplyWhileDragging }, isUnmounted, dragTarget, classTarget, mouseDownHandler, isDragging } = state;
+  let { config: { isDisabled: prevIsDisabled, classToApplyWhileDragging: prevClassToApplyWhileDragging }, dragTarget: prevDragTarget, classTarget: prevClassTarget,
+    isUnmounted: prevIsUnmounted, isDragging: prevIsDragging, removeHandlers: prevRemoveHandlers } = prevState;
+  const isEnabled = !isDisabled && !isUnmounted;
+  const prevIsEnabled = !prevIsDisabled && !prevIsUnmounted;
+  classTarget = classTarget || dragTarget;
+  prevClassTarget = prevClassTarget || prevDragTarget;
+
+  if (classTarget !== prevClassTarget || classToApplyWhileDragging !== prevClassToApplyWhileDragging || isEnabled !== prevIsEnabled || isDragging !== prevIsDragging) {
+    if (prevClassTarget && prevClassToApplyWhileDragging) { prevClassTarget.classList.remove(prevClassToApplyWhileDragging); }
+    if (isEnabled && isDragging && classTarget) { classTarget.classList.add(classToApplyWhileDragging); }
+  }
+  if (dragTarget !== prevDragTarget || isEnabled !== prevIsEnabled) {
+    prevRemoveHandlers();
+    if (prevDragTarget) { prevDragTarget.removeEventListener('mousedown', mouseDownHandler); }
+    if (isEnabled && dragTarget) { dragTarget.addEventListener('mousedown', mouseDownHandler); }
+  }
+
+  return state;
 }
 
 function getCoordinatesDiff(event: MouseEvent, coordinates: ICoordinates): ICoordinates {
@@ -25,15 +61,19 @@ function getCoordinatesDiff(event: MouseEvent, coordinates: ICoordinates): ICoor
   };
 }
 
-function dragStartHandler<TData, TPassthroughData>(config: IUseOnDragConfig<TData, TPassthroughData>, target: HTMLElement,
-  setIsDragging: (isDragging: boolean) => void, enabledRef: IUseToggleStateResult, event: MouseEvent): void {
-  const { threshold, onDragStart, onDrag, onDragEnd, data } = config;
+function dragStartHandler(state: IState, setState: SetState, event: MouseEvent): void {
   if (event.button !== 0) { return; }
   event.stopPropagation();
+  const { config: { threshold, onDragStart, onDrag, onDragEnd, data }, dragTarget: target } = state;
   const coordinates: ICoordinates = { x: event.clientX, y: event.clientY };
   const currentTarget = event.target as HTMLElement;
   let passthroughData = null;
   let startedDragging = false;
+
+  const removeHandlers = () => {
+    document.removeEventListener('mousemove', dragHandler);
+    document.removeEventListener('mouseup', dragEndHandler);
+  };
 
   const dragHandler = (dragEvent: MouseEvent) => {
     dragEvent.stopPropagation();
@@ -41,7 +81,7 @@ function dragStartHandler<TData, TPassthroughData>(config: IUseOnDragConfig<TDat
     if (!startedDragging) {
       if (Math.abs(coordinatesDiff.x) <= threshold && Math.abs(coordinatesDiff.y) <= threshold) { return; }
       startedDragging = true;
-      setIsDragging(true);
+      setState({ isDragging: true, removeHandlers });
       setDraggingData([data]);
       passthroughData = onDragStart({ target, currentTarget, coordinates, data });
     }
@@ -58,78 +98,35 @@ function dragStartHandler<TData, TPassthroughData>(config: IUseOnDragConfig<TDat
       passthroughData = null;
       removeHandlers();
       startedDragging = false;
-      setIsDragging(false);
+      setState({ isDragging: false, removeHandlers: () => void 0 });
       setDraggingData(null);
     }
   };
-
-  const removeHandlers = () => {
-    document.removeEventListener('mousemove', dragHandler);
-    document.removeEventListener('mouseup', dragEndHandler);
-  };
-
-  enabledRef.onDisable(removeHandlers);
 
   document.addEventListener('mousemove', dragHandler);
   document.addEventListener('mouseup', dragEndHandler);
 }
 
-function createDragTarget<TData, TPassthroughData>(dragTargetRef: HTMLElementRef, dragClassTargetRef: HTMLElementRef, config: IUseOnDragConfig<TData, TPassthroughData>,
-  setState: Dispatch<SetStateAction<IState>>, enabledRef: IUseToggleStateResult): HTMLTargetDelegate {
-  const setIsDragging = (isDragging: boolean) => {
-    setState(s => ({ ...s, isDragging }));
-    applyClassToElement(dragClassTargetRef.current || dragTargetRef.current, config.classToApplyWhileDragging, isDragging);
-  };
-  const boundDragStartHandler = useBound((event: MouseEvent) => dragStartHandler(config, dragTargetRef.current, setIsDragging, enabledRef, event));
-  const connected = (element: HTMLElement) => {
-    enabledRef.onChange(() => disconnected(element)); // if made disabled
-    dragTargetRef.current = element;
-    element.addEventListener('mousedown', boundDragStartHandler);
-  };
-  const disconnected = (element: HTMLElement) => {
-    enabledRef.onChange(() => connected(element)); // if made enabled
-    dragTargetRef.current = undefined;
-    element.removeEventListener('mousedown', boundDragStartHandler);
-  };
-  return useSingleDOMRef({ connected, disconnected });
-}
-
-function createDragClassTarget<TData, TPassthroughData>(dragClassTargetRef: HTMLElementRef, config: IUseOnDragConfig<TData, TPassthroughData>, isDragging: boolean) {
-  const connected = (element: HTMLElement) => {
-    dragClassTargetRef.current = element;
-    applyClassToElement(element, config.classToApplyWhileDragging, isDragging);
-  };
-  const disconnected = (element: HTMLElement) => {
-    applyClassToElement(element, config.classToApplyWhileDragging, false);
-    dragClassTargetRef.current = undefined;
-  };
-  return useSingleDOMRef({ connected, disconnected });
-}
-
 export function useOnDrag<TData = void, TPassthroughData = void>(config?: IUseOnDragConfig<TData, TPassthroughData>): IUseOnDragResult {
-  const [state, setState] = useState<IState>({
+  config = applyDefaults(config);
+  const [state, setState, onStateUpdate] = useStaticState<IState>({
     isDragging: false,
+    config,
+    dragTarget: undefined,
+    classTarget: undefined,
+    isUnmounted: false,
+    mouseDownHandler: event => dragStartHandler(state, setState, event),
+    removeHandlers: () => void 0,
   });
-  const dragTargetRef = useRef<HTMLElement>(undefined);
-  const dragClassTargetRef = useRef<HTMLElement>(undefined);
-  const enabledRef = useToggleState(config.isDisabled);
-  enabledRef.current = config.isDisabled;
+  onStateUpdate(handleStateUpdated);
+  setState({ config });
 
-  const internalConfig: IUseOnDragConfig<TData, TPassthroughData> = {
-    classToApplyWhileDragging: 'is-dragging',
-    threshold: 3,
-    onDragStart: () => null,
-    onDrag: ({ passthroughData }) => passthroughData,
-    onDragEnd: () => void 0,
-    ...config,
-  };
-
-  const dragTarget = createDragTarget(dragTargetRef, dragClassTargetRef, internalConfig, setState, enabledRef);
-  const dragClassTarget = createDragClassTarget(dragClassTargetRef, internalConfig, state.isDragging);
+  const dragTarget = useSingleDOMRef({ connected: element => setState({ dragTarget: element }), disconnected: () => setState({ dragTarget: undefined }) });
+  const dragClassTarget = useSingleDOMRef({ connected: element => setState({ classTarget: element }), disconnected: () => setState({ classTarget: undefined }) });
 
   useOnUnmount(() => {
-    enabledRef.current = false;
-    enabledRef.dispose();
+    useBound.disposeOf([dragTarget, dragClassTarget]);
+    setState({ isUnmounted: true });
   });
 
   return {
