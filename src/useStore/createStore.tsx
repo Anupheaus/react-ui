839b92b114ce8c7d292372a2f024a2d78569af38
Anupheaus <tony.hales@moodinternational.com>
2019-06-06 23:44:44 +0100

    Added more properties to the store component

--- src/useStore/createStore.tsx
+++ src/useStore/createStore.tsx
@@ -1,28 +1,48 @@
 import { IMap, is } from 'anux-common';
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useState } from 'react';
 import { Store } from './store';
 import { StoreContext } from './context';
 import { useOnUnmount } from '../useOnUnmount';
 import { StoreTypeId, IStore, IProviderProps, StoreActionsDelegate } from './models';
+import { useOnMount } from '../useOnMount';
+import { useToggleState } from '../useToggleState';
 
 export function createStore<TData extends IMap, TActions extends IMap>(initialData: TData, actions: StoreActionsDelegate<TData, TActions>) {
   const storeTypeId = Math.uniqueId();
 
-  const Provider: FunctionComponent<IProviderProps<TData>> = ({ children, initialData: providerInitialData, onChanged }) => {
+  const Provider: FunctionComponent<IProviderProps<TData, TActions>> = ({ children, initialData: providerInitialData, onChanged, onLoad, onLoading, onError }) => {
     const actualInitialData = is.function(providerInitialData) ? providerInitialData(initialData) : (providerInitialData || initialData);
     const store = new Store(actualInitialData, actions);
+    const [isLoading, setIsLoading] = useState(is.function(onLoad));
+    const [error, recordError] = useState<Error>();
+
+    onError = onError || ((displayError: Error) => (<div>An error has occurred within this store: {displayError.message}</div>));
+
     let unregisterOnChanged: () => void;
     if (onChanged) {
       unregisterOnChanged = store.register(onChanged);
     }
     const storeId = store.id;
 
+    useOnMount(async () => {
+      if (onLoad) {
+        try {
+          await onLoad(store.actions, store.data);
+        } catch (error) {
+          recordError(error);
+        } finally {
+          setIsLoading(false);
+        }
+      }
+    });
+
     useOnUnmount(() => {
       if (unregisterOnChanged) { unregisterOnChanged(); unregisterOnChanged = null; }
       store.dispose();
     });
 
-    return <StoreContext.Provider value={{ [storeTypeId]: storeId }}>{children || null}</StoreContext.Provider>;
+    const content = !is.null(error) ? onError(error) : isLoading && is.function(onLoading) ? onLoading() : (children || null);
+    return <StoreContext.Provider value={{ [storeTypeId]: storeId }}>{content}</StoreContext.Provider>;
   };
 
   Provider[StoreTypeId] = storeTypeId;
--- src/useStore/models.ts
+++ src/useStore/models.ts
@@ -1,14 +1,17 @@
-import { IMap, DeepPartial } from 'anux-common';
-import { FunctionComponent } from 'react';
+import { IMap, DeepPartial, PromiseMaybe } from 'anux-common';
+import { FunctionComponent, ReactNode } from 'react';
 
 export type StoreCallback<TData extends IMap = IMap> = (data: TData) => void;
 
-export interface IProviderProps<TData extends IMap> {
+export interface IProviderProps<TData extends IMap, TActions extends IMap> {
   initialData?: TData | ((initialData: TData) => TData);
   onChanged?(data: TData): void;
+  onLoad?(actions: TActions, data: TData): PromiseMaybe;
+  onLoading?(): ReactNode;
+  onError?(error: Error): ReactNode;
 }
 
-export interface IStore<TData extends IMap, TActions extends IMap> extends FunctionComponent<IProviderProps<TData>> {
+export interface IStore<TData extends IMap, TActions extends IMap> extends FunctionComponent<IProviderProps<TData, TActions>> {
   dataType: TData;
   actionsType: TActions;
   storeTypeId: string;
