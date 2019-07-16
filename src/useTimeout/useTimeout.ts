8a7ef60a57bbdb210ce80e8dd0821e03d655ed06
Tony Hales <tony.hales@moodinternational.com>
2019-07-16 11:04:14 +0100

    Modified useTimeout to use dependencies to re-trigger timeout if required

--- src/useTimeout/useTimeout.ts
+++ src/useTimeout/useTimeout.ts
@@ -1,24 +1,33 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 import { useOnUnmount } from '../useOnUnmount';
-import { useBound } from '../useBound';
+import { useBinder } from '../useBinder';
+import { useSharedHookState } from '../useSharedHookState';
 
 interface IOptions {
   triggerOnUnmount?: boolean;
+  dependencies?: any[];
 }
 
 export function useTimeout(delegate: () => void, timeout: number, options?: IOptions): () => void {
   options = {
     triggerOnUnmount: false,
+    dependencies: [],
     ...options,
   };
 
   const timeoutRef = useRef(null);
-  timeoutRef.current = setTimeout(delegate, timeout);
+  const sharedHookState = useSharedHookState();
+  const bind = useBinder(sharedHookState);
 
-  const cancelTimeout = useBound(() => {
+  const cancelTimeout = bind(() => {
     if (timeoutRef.current) { clearTimeout(timeoutRef.current); }
     timeoutRef.current = null;
   });
+
+  useEffect(() => {
+    cancelTimeout();
+    timeoutRef.current = setTimeout(delegate, timeout);
+  }, options.dependencies);
 
   useOnUnmount(() => {
     if (options.triggerOnUnmount && timeoutRef.current) { delegate(); }
