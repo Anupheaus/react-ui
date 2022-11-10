b8e8b49a43df2cd556df605ec668feeb985a9c75
Tony Hales <tony.hales@revlifter.com>
2022-11-10 00:51:41 +0000

    Updated the package.json to add storybook as a dep instead of a devDep

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,8 @@
 import './extensions';
 
-// export * from './children';
 export * from './components';
-// export * from './anuxComponents';
-// export * from './events';
 export * from './errors';
-// export * from './anuxContext';
-// export * from './EventBoundary';
 export * from './theme';
-// export * from './placeholders';
 export * from './components';
 export * from './Storybook';
 export * from './models';
